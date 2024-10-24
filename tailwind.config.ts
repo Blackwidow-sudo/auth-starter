8ededb53afdfc9b251172f6cffcc7261fc0f8b59
Blackwidow-sudo <bob_merlin92@web.de>
2024-10-24 22:18:44 +0200

    feat: add darkmode option to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import typography from '@tailwindcss/typography'
 import type { Config } from 'tailwindcss'
 
 export default {
+	darkMode: 'media',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
