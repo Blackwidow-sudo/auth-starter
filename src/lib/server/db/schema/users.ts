055aed44c087749c2882f806855a6b21eafa9c25
Blackwidow-sudo <bob_merlin92@web.de>
2024-10-24 21:58:27 +0200

    fix(users): rename column name for email

--- src/lib/server/db/schema/users.ts
+++ src/lib/server/db/schema/users.ts
@@ -3,7 +3,7 @@ import { pgTable, text, integer, timestamp, varchar } from 'drizzle-orm/pg-core'
 export const users = pgTable('users', {
 	id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
 	username: varchar('username', { length: 256 }).notNull(),
-	email: varchar('username', { length: 256 }).notNull().unique(),
+	email: varchar('email', { length: 256 }).notNull().unique(),
 	passwordHash: text('password_hash').notNull()
 })
 
