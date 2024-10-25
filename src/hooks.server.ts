25e4416c80198ef1b792d029badae0f7a32db79f
Blackwidow-sudo <bob_merlin92@web.de>
2024-10-25 20:45:02 +0200

    fix(hooks.server.ts): remove unused hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,8 @@
 import { dev } from '$app/environment'
-import { routes } from '$lib/routes'
 import * as auth from '$lib/server/auth'
-
-import { error, redirect, type Handle } from '@sveltejs/kit'
 import { sequence } from '@sveltejs/kit/hooks'
+
+import type { Handle } from '@sveltejs/kit'
 
 const handleAuth: Handle = async ({ event, resolve }) => {
 	const sessionId = event.cookies.get(auth.sessionCookieName)
@@ -30,29 +29,6 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 
 	event.locals.user = user
 	event.locals.session = session
-
-	return resolve(event)
-}
-
-// FIXME: Error gets thrown on /signup and /login
-const handleRoutes: Handle = async ({ event, resolve }) => {
-	const authRoutes = ['/signup', '/login']
-	const targetRoute = routes.find(({ path }) => path === event.url.pathname)
-	const isAuthRoute = authRoutes.includes(event.url.pathname)
-
-	if (!targetRoute || !isAuthRoute) {
-		return error(404, `${event.url.pathname} not found. Is auth route? ${isAuthRoute}`)
-	}
-
-	const authenticated = !!event.locals.user
-
-	if (authenticated && authRoutes.includes(targetRoute.path)) {
-		return redirect(302, '/')
-	}
-
-	if (!authenticated && !targetRoute.public) {
-		return redirect(302, '/login')
-	}
 
 	return resolve(event)
 }
