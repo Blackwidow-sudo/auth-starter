9de922b4950044ef8defb4dcc0bd78f639380f45
Blackwidow-sudo <bob_merlin92@web.de>
2024-10-31 23:24:24 +0100

    feat: add basic fallback error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+</script>
+
+<div class="space-y-4 p-4">
+	<h1 class="text-center text-xl font-semibold text-red-600 dark:text-red-400">
+		{$page.status}: {$page.error?.message}
+	</h1>
+</div>
