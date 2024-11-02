6c1b4b2377725abf0b5fcc16b7036e2d3cc042ab
Blackwidow-sudo <bob_merlin92@web.de>
2024-11-02 21:37:03 +0100

    fix: rmv outcommented code in navigation component

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,40 +1,29 @@
 <script lang="ts">
-	import { cn } from '$lib/utils'
 	import { page } from '$app/stores'
 
 	import type { Route } from '$lib/routes'
+	import { cn } from '$lib/utils'
+	import type { HTMLAttributes } from 'svelte/elements'
 
-	interface Props {
-		[key: string]: unknown
-		class?: HTMLElement['className']
+	interface Props extends HTMLAttributes<HTMLElement> {
 		routes: Route[]
 	}
 
 	let { routes, class: cls = '', ...restProps }: Props = $props()
 </script>
 
-<nav>
+<nav
+	class={cn('w-full', cls)}
+	{...restProps}>
 	<ul class="menu menu-lg px-4 py-0 lg:menu-md">
 		{#each routes as route (route.path)}
 			<li>
 				<a
 					href={route.path}
-					class:active={$page.url.pathname === route.path}>{route.name}</a>
+					class:active={$page.url.pathname === route.path}>
+					{route.name}
+				</a>
 			</li>
 		{/each}
 	</ul>
 </nav>
-
-<!-- <nav
-	class={cn('grid min-w-36 items-center bg-slate-300 py-1 dark:bg-slate-900', cls)}
-	{...restProps}>
-	{#each routes as route (route.path)}
-		<a
-			class="block text-nowrap px-4 py-2 transition hover:bg-slate-100 dark:text-slate-200 dark:hover:bg-slate-700"
-			class:bg-slate-200={$page.url.pathname === route.path}
-			class:dark:bg-slate-800={$page.url.pathname === route.path}
-			href={route.path}>
-			{route.name}
-		</a>
-	{/each}
-</nav> -->
